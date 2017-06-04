6583e3e2d238eaa4e6602f716afe71510de55b49
tjpc3 <hotwire567@gmail.com>
2017-06-04 10:06:41 -0400

    dg

--- eventHandling/newUser.js
+++ eventHandling/newUser.js
@@ -3,7 +3,12 @@ const logUtil = require("../util/logging.js");
 module.exports = function(client) {
   client.on("guildMemberAdd", function(member) {
     logUtil.log(member.user.tag + " has joined a guild!", logUtil.STATUS_INFO);
-    member.addRole("318560521802481665");
+    try {
+      member.addRole("318560521802481665");
+    } catch (exception) {
+      logUtil("Exception while adding role to new user.", logUtil.STATUS_WARNING);
+      console.log(exception);
+    }
     member.guild.defaultChannel.send("Welcome " + member.user.toString() + " to your new robot masters for all of eternity!");
   });
 }
--- util/reddit.js
+++ util/reddit.js
@@ -1,14 +1,23 @@
 module.exports = {};
 
-var request = require('request');
+const request = require('request');
+const logUtil = require('./logging.js');
 
 module.exports.getRandomUrl = function(subreddit, callback) {
   if (subreddit.indexOf('?') != -1) {
     callback(null);
     return;
   }
+
   request("https://www.reddit.com/r/" + subreddit + "/hot/.json", function(error, response, body) {
-    var listing = JSON.parse(body);
+    try {
+      var listing = JSON.parse(body);
+    } catch (exception) {
+      logUtil.log("Error parsing JSON data for !m command.", logUtil.STATUS_WARNING);
+      console.log(exception);
+      callback(null);
+      return;
+    }
     if (listing.data != null) {
       var children = listing.data.children; // Get list
       var post = children[getRandomInt(1, 25)];
