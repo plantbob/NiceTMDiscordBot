3057dfaf4c0922452c4a14eb7b3a347ac3ea694b
tjpc3 <hotwire567@gmail.com>
2017-06-05 16:01:15 -0400

    xsdhnj

--- util/reddit.js
+++ util/reddit.js
@@ -1,32 +1,51 @@
 module.exports = {};
 
 const request = require('request');
+const nodecache = require('node-cache');
+const cache = new nodecache();
+
 const logUtil = require('./logging.js');
 
-module.exports.getRandomUrl = function(subreddit, callback) {
+module.exports.getRandomUrl = function(subreddit, callback) { // TODO: Add limit to how many listings can be cached
   if (subreddit.indexOf('?') != -1) {
     callback(null);
     return;
   }
 
-  request("https://www.reddit.com/r/" + subreddit + "/hot/.json", function(error, response, body) {
-    try {
-      var listing = JSON.parse(body);
-    } catch (exception) {
-      logUtil.log("Error parsing JSON data for !m command.", logUtil.STATUS_WARNING);
-      console.log(exception);
-      callback(null);
-      return;
-    }
+  var listing = cache.get(subreddit); // See if listing is cached in memory
+  if (listing != undefined) {
     if (listing.data != null) {
       var children = listing.data.children; // Get list
-      var post = children[getRandomInt(1, 25)];
+      var post = children[getRandomInt(1, children.length)];
       if (post && post.kind && post.kind == "t3") { // Make sure post is a link
         var postdata = post.data;
         callback(postdata.url);
         return;
       }
     }
+  }
+
+  request("https://www.reddit.com/r/" + subreddit + "/hot/.json?limit=250", function(error, response, body) {
+    try {
+      var listing = JSON.parse(body);
+    } catch (exception) {
+      logUtil.log("Invalid JSON data recieved from Reddit.", logUtil.STATUS_WARNING);
+      callback(null);
+      return;
+    }
+
+    cache.set(subreddit, listing, 3600); // Listing expires after one hour
+
+    if (listing.data != null) {
+      var children = listing.data.children; // Get list
+      var post = children[getRandomInt(1, children.length)];
+      if (post && post.kind && post.kind == "t3") { // Make sure post is a link
+        var postdata = post.data;
+        callback(postdata.url);
+        return;
+      }
+    }
+
     callback(null);
   });
 }
