b42096cc48184b54c27b2bd91528f68c57585d82
tjpc3 <tjpc3@users.noreply.github.com>
2017-07-21 15:16:04 -0400

    n

--- eventHandling/command/reactionCommands.js
+++ eventHandling/command/reactionCommands.js
@@ -4,22 +4,24 @@ const Discord = require("discord.js");
 // Params = The parameters of the command
 // Globals = The global variables for the server that the command was initiated in
 var commands = {
-  "nice" : '™',
-  "deja vu?" : 'drift'
+  "nice" : function(message, params, globals) {
+    if (params[0] == null) {
+      message.react('™');
+    }
+  },
+  "deja" : function(message, params, globals) {
+    if (params[0].toLowerCase() == "vu?" && !params[1]) {
+      message.react("d");
+      message.react("r");
+      message.react("i");
+      message.react("f");
+      message.react("t");
+    }
+  }
 }
 
 module.exports.searchFunction = function(command) {
-  var lowerCase = command.toLowerCase();
-
-  if (commands[lowerCase]) {
-    return function(message, params, globals) {
-      if (params[0] == null) {
-        for (var i = 0; i < command[lowerCase].length; i++) {
-          message.react(commands[lowerCase][i]);
-        }
-      }
-    };
-  }
+  return commands[command.toLowerCase()];
 }
 
 module.exports.close = function(globals, guild) {
