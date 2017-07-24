80f7da6c83f04de4351b43d6aa3eb74ae9def36e
tjpc3 <tjpc3@users.noreply.github.com>
2017-07-24 10:22:54 -0400

    bug fix

--- eventHandling/nickname.js
+++ eventHandling/nickname.js
@@ -2,8 +2,8 @@ module.exports = {};
 
 module.exports.init = function(client) {
   client.on('ready', function() {
-    for (var i = 0; i < client.guilds.length; i++) {
-      var guild = client.guilds[i];
+    for (var i = 0; i < client.guilds.size; i++) {
+      var guild = client.guilds.get(i);
 
       if (guild.me.nickname == "nice-tm-bot") {
         try {
