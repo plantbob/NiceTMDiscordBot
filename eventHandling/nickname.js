7b7af1046768cfeaf0b69a57c89fc946f1a69205
tjpc3 <tjpc3@users.noreply.github.com>
2017-07-24 10:16:33 -0400

    bug fix

--- eventHandling/nickname.js
+++ eventHandling/nickname.js
@@ -1,6 +1,6 @@
 module.exports = {};
 
-module.exports.init = function() {
+module.exports.init = function(client) {
   client.on('ready', function() {
     for (var i = 0; i < client.guilds.length; i++) {
       var guild = client.guilds[i];
