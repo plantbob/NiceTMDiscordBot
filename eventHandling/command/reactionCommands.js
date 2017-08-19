0e59177dbe2a20b72e62e73effb731c45b9e0e6d
tjpc3 <tjpc3@users.noreply.github.com>
2017-08-19 17:23:35 -0400

    improved nice command

--- eventHandling/command/reactionCommands.js
+++ eventHandling/command/reactionCommands.js
@@ -4,11 +4,6 @@ const Discord = require("discord.js");
 // Params = The parameters of the command
 // Globals = The global variables for the server that the command was initiated in
 var commands = {
-  "nice" : function(message, params, globals) {
-    if (params[0] == null) {
-      message.react('™');
-    }
-  },
   "deja" : function(message, params, globals) {
     if (params[0].toLowerCase() == "vu?" && !params[1]) {
       message.react("🇩").then(function() {
@@ -24,7 +19,37 @@ var commands = {
   }
 }
 
+function reactWithTM(message, params, globals) {
+  if (params[0] == null) {
+    message.react('™');
+  }
+}
+
 module.exports.searchFunction = function(command) {
+  if (command[0] == 'n' && command.length >= 4) {
+    var isNice = false;
+    var lastChar = 'n';
+    for (var i = 1; i < command.length; i++) {
+      var currentChar = command[i];
+
+      if (lastChar == currentChar) {
+      } else if (lastChar == 'n' && currentChar == 'i') {
+      } else if (lastChar == "i" && currentChar == "c") {
+      } else if (lastChar == "c" && currentChar == "e") {
+        isNice = true;
+      } else {
+        isNice = false;
+        break;
+      }
+
+      lastChar = currentChar;
+    }
+
+    if (isNice) {
+      return reactWithTM;
+    }
+  }
+
   return commands[command.toLowerCase()];
 }
 
