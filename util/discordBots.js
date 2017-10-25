92ac613233957a8827c0e01a38c733fcc1807a9c
tjpc3 <tjpc3@no-reply.github.com>
2017-10-24 21:18:14 -0400

    added api support for another discord bots website

--- util/discordBots.js
+++ util/discordBots.js
@@ -4,13 +4,22 @@ const request = require('request');
 const token = require("../config/token.js");
 
 module.exports.postGuildCount = function(count, callback) {
-    request({
-        method: "POST",
-        json: true,
-        headers: {
-          Authorization: token.discordBotsToken
-        },
-        url: 'https://bots.discord.pw/api/bots/318558676241874945/stats',
-        body: {"server_count": count}
-      }, callback);
+  request({
+      method: "POST",
+      json: true,
+      headers: {
+        Authorization: token.discordBotsToken
+      },
+      url: 'https://bots.discord.pw/api/bots/318558676241874945/stats',
+      body: {"server_count": count}
+    }, callback);
+  request({
+    method: "POST",
+    json: true,
+    headers: {
+      Authorization: token.discordBots2Token
+    },
+    url: 'https://discordbots.org/api/bots/318558676241874945/stats',
+    body: {"server_count": count}
+  }, callback);
 }
