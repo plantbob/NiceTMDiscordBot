719f9c106c044b5449b4d18f7a0de05e704ed2a5
tjpc3 <tjpc3@users.noreply.github.com>
2017-12-27 21:58:17 -0500

    increase steampasses

--- config/misc.js
+++ config/misc.js
@@ -1,3 +1,3 @@
 module.exports = {};
 
-module.exports.playStreamPasses = 1; // The passes parameter in the PlayStream method from voiceConnections
+module.exports.playStreamPasses = 2; // The passes parameter in the PlayStream method from voiceConnections
